1644476e530cb357705d8b01d341cadb7cf8e889
ASE-CHSU <chsu@ase-edv.eu>
2025-07-21 19:02:30 +0200

    SmallChange

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Crypet99.github.io/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

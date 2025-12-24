494a60f53785757235b7e4b4fe9e93b3a1a7b33f
ASE-CHSU <chsu@ase-edv.eu>
2025-12-24 18:57:36 +0100

    Githup Pages Build

--- XMAS-Calender/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomesComponent } from './homes/homes.component';
-import { GiftsComponent } from './gifts/gifts.component';
-
-export const routes: Routes = [
-	{ path: '', component: HomesComponent },
-	{ path: 'gift/:id', component: GiftsComponent },
-	{ path: '**', redirectTo: '' }
-];
